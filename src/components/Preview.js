350ee9ed8d72ddf1495300f72990116cf0fffbb4
Ryan Smith 4 <ryan.smith4@nike.com>
2018-08-08 17:23:47 -0700

    Re-init mermaid after images loaded. Not sure if it's helping...

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -34,9 +34,27 @@ class Preview extends React.Component {
 
   initMermaid () {
     const { code, history, match: { url } } = this.props
+
+    const firstLoadCallback = (...args) => {
+      const images = this.container.getElementsByTagName('img');
+      const imageCount = images.length;
+      if(images && imageCount) {
+        let loadedCount = 0;
+        for(const image of images){
+          image.onload = () => {
+            loadedCount++;
+            if(loadedCount >= imageCount){
+              // all images loaded, re-init
+              mermaid.init(undefined, this.container);
+            }
+          };
+        }
+      }
+    };
+
     try {
       mermaid.parse(code)
-      mermaid.init(undefined, this.container)
+      mermaid.init(undefined, this.container, firstLoadCallback)
     } catch (e) { // {str, hash}
       const base64 = Base64.encodeURI(e.str || e.message)
       history.push(`${url}/error/${base64}`)
